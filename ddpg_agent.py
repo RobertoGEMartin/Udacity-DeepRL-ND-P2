0a83ac920484a6c52f98ed5d73f4192ab7b1588a
@rgem52 <rgomez@hi-iberia.es>
2018-11-14 09:49:01 +0100

    checkpoints folder, new hyperparameters

--- ddpg_agent.py
+++ ddpg_agent.py
@@ -9,13 +9,25 @@ import torch
 import torch.nn.functional as F
 import torch.optim as optim
 
-BUFFER_SIZE = int(1e5)  # replay buffer size
-BATCH_SIZE = 128        # minibatch size
+'''
+#######################################################################################
+References
+1. [Udacity Gihub Repo](https://github.com/udacity/deep-reinforcement-learning)
+2. [Unity Docs](https://github.com/Unity-Technologies/ml-agents/blob/master/docs/ML-Agents-Overview.md)
+3. [Unity Paper](https://arxiv.org/abs/1809.02627)
+4. [openai master RL](https://spinningup.openai.com/en/latest/algorithms/ddpg.html)
+5. [Original paper](https://arxiv.org/abs/1509.02971)
+6. [openai Baselines](https://blog.openai.com/better-exploration-with-parameter-noise/)
+#######################################################################################
+'''
+
+BUFFER_SIZE = int(1e6)  # replay buffer size
+BATCH_SIZE = 1024       # minibatch size before 128
 GAMMA = 0.99            # discount factor
 TAU = 1e-3              # for soft update of target parameters
-LR_ACTOR = 1e-4         # learning rate of the actor 
-LR_CRITIC = 1e-3        # learning rate of the critic
-WEIGHT_DECAY = 0        # L2 weight decay
+LR_ACTOR = 1e-3         # learning rate of the actor 
+LR_CRITIC = 1e-3        # learning rate of the critic before: 3e-4
+WEIGHT_DECAY = 0.0000   # L2 weight decay
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
